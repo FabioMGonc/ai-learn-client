2121f42c700ea1c853554b891e12330de76dd381
Fabio M. Gonçalves <167380233+FabioMGonc@users.noreply.github.com>
2026-08-24 00:35:18 -0300

    Configurando os filtros de professor

--- components/ui/TutorFilters.tsx
+++ components/ui/TutorFilters.tsx
@@ -1,7 +1,63 @@
+"use client"
+import { useRouter, useSearchParams } from "next/navigation";
+import { useState, useEffect, useRef } from "react";
+import Title from "../general/Title";
 
 const TutorFilters = () => {
+    const router = useRouter();
+    const searchParams = useSearchParams();
+
+    const subject = searchParams.get("subject") || "all";
+    const topic = searchParams.get("topic") || "";
+    const [ query, setQuery ] = useState(topic);
+    const isFirstRender = useRef(true);
+
+    useEffect(() => {
+        setQuery(topic);
+    }, [topic]);
+
+    useEffect(() => {
+        if(isFirstRender.current) {
+            isFirstRender.current = false;
+            return;
+        }
+        
+        const timer = setTimeout(() => {
+            const params = new URLSearchParams(searchParams.toString());
+            if(query) {
+                params.set("topic", query);
+            } else {
+                params.delete("topic");
+            }
+            router.replace(`/tutors?${params.toString()}`, { scroll: false });
+        }, 500);
+        return () => clearTimeout(timer);
+    }, [query]);
+
+    const handleSubjectChange = (value: string) => {
+        const params = new URLSearchParams(searchParams.toString());
+
+        if(value === "all") {
+            params.delete("subject");
+            params.delete("topic");
+            setQuery("");
+        } else {
+           params.set("subject", value);
+        }
+        router.replace(`/tutors?${params.toString()}`, { scroll: false });
+    }
+
+    const clearSearch = () => {
+        setQuery("");
+        const params = new URLSearchParams(searchParams.toString());
+        params.delete("topic");
+        router.replace(`/tutors?${params.toString()}`, { scroll: false });
+    };
+
     return (
-        <div className="space-y-4">TutorFilters</div>
+        <div className="flex flex-col gap-6">
+            <Title headingStart="Procure" headingEnd="professores" subtext="Filtre suas buscas para encontrar os melhores professores disponíveis para você"/>
+        </div>
     );
 }
 
