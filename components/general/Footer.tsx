2b913d5d0cd100b9d286441d30a79defbe8c8baa
Fabio Gonçalves <fabiothewarrior@hotmail.com>
2026-08-31 11:31:30 -0300

    Atualização footer

--- components/general/Footer.tsx
+++ components/general/Footer.tsx
@@ -2,7 +2,7 @@
 const Footer = () => {
     return (
         <footer className="border-t border-zinc-300 py-6 text-center text-sm text-zinc-700">
-            © {new Date().getFullYear()} Criado por Fabio Gonçalves. Todos os direitos reservados.
+            © {new Date().getFullYear()} Criado por <a href="https://www.linkedin.com/in/fabio-m-gonc/">Fabio Gonçalves</a>. Todos os direitos reservados.
         </footer>
     );
 }
